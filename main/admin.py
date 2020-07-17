979dd59232e6c841a272d14eb54886c6966988a9
rghvmandowara@gmail.com <RghvMandowara@gmail.com>
2020-07-17 13:08:57 +0530

    home page design 1 commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Gallery,Blog
 # Register your models here.
+admin.site.register(Gallery)
+admin.site.register(Blog)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+	image = models.ImageField(upload_to ='media/gallery/')
+	description = models.CharField(max_length=100)
+	time = models.DateTimeField(auto_now_add=True)
+class Blog(models.Model):
+	image=models.ImageField(upload_to='media/blog/')
+	description=models.CharField(max_length=400)
+	time = models.DateTimeField(auto_now_add=True)
