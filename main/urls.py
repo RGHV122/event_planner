9e2fce871e90231f48d8b08db7ef52069d77d9d0
rghvmandowara@gmail.com <RghvMandowara@gmail.com>
2020-07-19 00:47:36 +0530

    Service page basic layout started not done yet

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name='main'
 urlpatterns = [
-
     path('', views.index,name='index'),
+    path('services/',views.services,name='services')
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'main/index.html')
+
+def services(request):
+	colors=['#0275d8','#5cb85c','#5bc0de','#f0ad4e','#d9534f']
+	return render(request,'main/services.html',{'colors':colors})
