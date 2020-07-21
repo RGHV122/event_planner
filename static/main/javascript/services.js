c39d36e9f60d87a47ad3412eae288d00af0a2452
rghvmandowara@gmail.com <RghvMandowara@gmail.com>
2020-07-21 23:42:55 +0530

    no message

---
+++ static/main/javascript/services.js
@@ -0,0 +1,43 @@
+function social_media_facebook(){
+	var facebook_span=document.getElementsByClassName("social-media-facebook")[0];
+	var instagram_span=document.getElementsByClassName("social-media-instagram")[0];
+	var youtube_span=document.getElementsByClassName("social-media-youtube")[0];
+	var whatsapp_span=document.getElementsByClassName("social-media-whatsapp")[0];
+	facebook_span.style.display="block";
+	instagram_span.style.display="none";
+	youtube_span.style.display="none";
+	whatsapp_span.style.display="none";
+
+}
+function social_media_instagram() {
+	var facebook_span=document.getElementsByClassName("social-media-facebook")[0];
+	var instagram_span=document.getElementsByClassName("social-media-instagram")[0];
+	var youtube_span=document.getElementsByClassName("social-media-youtube")[0];
+	var whatsapp_span=document.getElementsByClassName("social-media-whatsapp")[0];
+	facebook_span.style.display="none";
+	instagram_span.style.display="block";
+	youtube_span.style.display="none";
+	whatsapp_span.style.display="none";
+}
+function social_media_youtube(){
+	var facebook_span=document.getElementsByClassName("social-media-facebook")[0];
+	var instagram_span=document.getElementsByClassName("social-media-instagram")[0];
+	var youtube_span=document.getElementsByClassName("social-media-youtube")[0];
+	var whatsapp_span=document.getElementsByClassName("social-media-whatsapp")[0];
+	facebook_span.style.display="none";
+	instagram_span.style.display="none";
+	youtube_span.style.display="block";
+	whatsapp_span.style.display="none";
+
+}
+function social_media_whatsapp(){
+	var facebook_span=document.getElementsByClassName("social-media-facebook")[0];
+	var instagram_span=document.getElementsByClassName("social-media-instagram")[0];
+	var youtube_span=document.getElementsByClassName("social-media-youtube")[0];
+	var whatsapp_span=document.getElementsByClassName("social-media-whatsapp")[0];
+	facebook_span.style.display="none";
+	instagram_span.style.display="none";
+	youtube_span.style.display="none";
+	whatsapp_span.style.display="block";
+
+}
