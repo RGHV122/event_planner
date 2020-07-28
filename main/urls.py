0e11d36a33ea35ad1dab80820a96021f94ea0541
rghvmandowara@gmail.com <RghvMandowara@gmail.com>
2020-07-28 23:20:27 +0530

    Raghav's part done, home,service,gallery done

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,10 @@ from . import views
 app_name='main'
 urlpatterns = [
     path('', views.index,name='index'),
-
     path("about/", views.about, name="about"),
+    path('services/',views.services,name='services'),
+    path('gallery/page=<page>',views.gallery,name='gallery'),
+    path('gallery/',views.gallery,name='gallery'),
 
-    path('services/',views.services,name='services')
 
 ]
