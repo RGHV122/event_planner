a80cebbb3f508e5909b24eb5846518673908de01
rghvmandowara@gmail.com <RghvMandowara@gmail.com>
2020-08-21 23:15:41 +0530

    slider in gallery styled

--- static/main/javascript/gallery.js
+++ static/main/javascript/gallery.js
@@ -4,9 +4,13 @@ function showMyImage(id) {
 	var image_in_popup = document.getElementById("image-in-popup");
 	modal.style.display = "block";
 	image_in_popup.src = image.src;
-	console.log("got ele");
+	var slider_indecator = document.getElementById("slider-indecator");
+	slider_indecator.style.display="none";
+
 }
 function hide_popup() {
 	var modal = document.getElementById("image-popup");
 	modal.style.display = "none";
+	var slider_indecator = document.getElementById("slider-indecator");
+	slider_indecator.style.display="block";
 }
